7b1b9c83153a1aa0fbcf04bb10817d2abdf18edd
ray <raymon.burke@gmail.com>
2024-04-17 03:36:52 +0000

    structure with setup.py and makefile

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups locally or to aws s3',
+    long_description=readme,
+    author='Ray',
+    author_email='ray@email.com',
+    install_requires=[],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
